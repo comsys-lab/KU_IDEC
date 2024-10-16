113a40aab2e84b758dce21f801c80a979eb0886c
minseong <ms7859@korea.ac.kr>
2024-10-16 09:45:40 +0900

    Compress and split data_alexnet and data_vgg16

--- Day2/restore_data.sh
+++ Day2/restore_data.sh
@@ -1,4 +1,20 @@
-cat data_alexnet_vgg16.tar.gz.part* > data_alexnet_vgg16.tar.gz
+#!/bin/bash
+
+current_dir=$(pwd)
+
+parent_dir=$(basename "$(dirname "$current_dir")")
+current_dir_name=$(basename "$current_dir")
+
+last_two_dirs="$parent_dir/$current_dir_name"
+expected_dirs="KU_IDEC/Day2"
+
+if [ "$last_two_dirs" != "$expected_dirs" ]; then
+  echo "Error: This script must be run from the KU_IDEC/Day2 directory."
+  exit 1
+fi
+
+cat split_files/data_alexnet_vgg16.tar.gz.part* > data_alexnet_vgg16.tar.gz
 tar -xvzf data_alexnet_vgg16.tar.gz
 
-rm data_alexnet_vgg16.tar.gz*
+rm -rf ./split_files
+rm -rf ./data_alexnet_vgg16.tar.gz
